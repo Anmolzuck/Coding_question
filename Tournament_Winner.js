0c12b3316dba77d8135c74e51ae77da99f142942
Anmol Zakie <anmolzuck17@gmail.com>
2021-05-13 12:05:26 +0530

    Tournament Winner

--- Tournament_Winner.js
+++ Tournament_Winner.js
@@ -1,3 +1,5 @@
+/*** This code takes O(nlogn)time because of sorting | O(k) Space */
+
 function tournamentWinner(competitions, results) {
   let newObj = { competitions };
   let newArray = [];
@@ -7,7 +9,6 @@ function tournamentWinner(competitions, results) {
     } else {
       newArray.push(newObj.competitions[i][0]);
     }
-    //console.log(newObj.competitions[0][1]);
   }
   return newArray
     .sort(
@@ -18,34 +19,30 @@ function tournamentWinner(competitions, results) {
     .pop();
 }
 
-console.log(
-  tournamentWinner(
-    // [
-    //   ["HTML", "PYTHON"],
-    //   ["C#", "PYTHON"],
-    //   ["PYTHON", "HTML"],
-    // ],
-    // [0, 0, 1]
-    [
-      ["HTML", "C#"],
-      ["C#", "Python"],
-      ["Python", "HTML"],
-    ],
-    [0, 0, 1]
-    // [
-    //   ["HTML", "Java"],
-    //   ["Java", "Python"],
-    //   ["Python", "HTML"],
-    //   ["C#", "Python"],
-    //   ["Java", "C#"],
-    //   ["C#", "HTML"],
-    //   ["SQL", "C#"],
-    //   ["HTML", "SQL"],
-    //   ["SQL", "Python"],
-    //   ["SQL", "Java"],
-    // ],
-    // [0, 1, 1, 1, 0, 1, 0, 1, 1, 0]
-  )
+/*** Best code O(n)time | O(k)space */
 
-  //tournamentWinner([["bulls", "eagles"]], [1])
-);
+const HOME_TEAM_WON = 1;
+
+function tournamentWinner(competitions, results) {
+  let currentBestTeam = "";
+  const scores = { [currentBestTeam]: 0 };
+
+  for (let i = 0; i < competitions.length; i++) {
+    const result = results[i];
+    const [homeTeam, awayTeam] = competitions[i];
+
+    const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+    updateScores(winningTeam, 3, scores);
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
+    }
+  }
+  return currentBestTeam;
+}
+
+function updateScores(team, points, scores) {
+  if (!(team in scores)) scores[team] = 0;
+  scores[team] += points;
+}
